a6a45431b9c4629800d9130279b325975d71dbcb
raghavendra singh <103146867+raghavendra7992@users.noreply.github.com>
2022-12-16 17:14:53 +0530

    prod

--- reliance-d/src/App.js
+++ reliance-d/src/App.js
@@ -3,13 +3,13 @@ import AllRoutes from "./Components/AllRoutes/AllRoutes"
 import Navbar from "./Components/Navbar/Navbar"
 import Footer from "./Components/Navbar/Footer"
 
+
 function App() {
   return (
     <div>
       <Navbar />
     <AllRoutes />
-    <Footer />
-
+    <Footer /> 
     </div>
     
   );
