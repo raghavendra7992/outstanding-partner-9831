8dfc439b541965bc33ce02f224fc57fff7e6db40
Abhis113 <abhishekranjangecd@gmail.com>
2022-12-17 21:52:44 +0530

    login_day_06

--- reliance-d/src/Components/AllRoutes/AllRoutes.jsx
+++ reliance-d/src/Components/AllRoutes/AllRoutes.jsx
@@ -7,9 +7,11 @@ import Pagenotfound from '../Pages/Pagenotfound'
 
 import Loginforadmin from '../Pages/Loginforadmin'
 import React from "react";
+import Register from "../Pages/Register";
 import { Route, Routes } from "react-router-dom";
 
-import Login from '../Pages/Login'
+// import Login from '../Pages/Login';
+import Login from '../Pages/Login1';
 
 import Home from "../Pages/Home";
 
@@ -19,6 +21,8 @@ const AllRoutes = () => {
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/login" element={<Login />}></Route>
+      <Route path="/login1" element={<Login />}></Route>
+      <Route path="/Register" element={<Register />}></Route>
       <Route path="/signup" element={<Signup />}></Route>
       <Route path="/cart" element={<Cart />}></Route>
       <Route path="/admin-login" element={<Loginforadmin />}></Route>
